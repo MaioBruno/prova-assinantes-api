dc63565d0be4cd6a5200264d8a404b1561938308
brunoMaio <bruno.maio@wansoft.com.br>
2026-07-08 00:18:08 -0300

    feature criacao migration

--- src/AssinantesApi/Data/AppDpContext.cs
+++ src/AssinantesApi/Data/AppDpContext.cs
@@ -11,5 +11,13 @@ namespace AssinantesApi.Data
 
         // Isso aqui cria a tabela no banco de dados baseada na sua classe Assinante
         public DbSet<Assinante> Assinantes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        // Aqui resolvemos o aviso do 'ValorMensal'
+        modelBuilder.Entity<Assinante>()
+            .Property(a => a.ValorMensal)
+            .HasPrecision(18, 2); // Define: 18 dígitos totais, sendo 2 após a vírgula
+        }
     }
 }
